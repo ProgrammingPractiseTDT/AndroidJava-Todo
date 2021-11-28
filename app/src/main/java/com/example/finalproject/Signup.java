45dc764c19f21181f74a7392b34e7603c166a1d9
ngoch <ngochuy11010@gmail.com>
2021-11-28 23:05:36 +0700

    Sửa tý, coi history tiếp đi

--- app/src/main/java/com/example/finalproject/Signup.java
+++ app/src/main/java/com/example/finalproject/Signup.java
@@ -7,10 +7,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -130,4 +132,5 @@ public class Signup extends AppCompatActivity {
                     }
                 });
     }
+
 }
