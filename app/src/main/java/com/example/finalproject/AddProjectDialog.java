34f4878f84422dfed63515dfedac48de5f86ae90
Ngoc Huy <ngochuy11010@gmail.com>
2021-12-15 00:57:29 +0700

    Add change color basic

--- app/src/main/java/com/example/finalproject/AddProjectDialog.java
+++ app/src/main/java/com/example/finalproject/AddProjectDialog.java
@@ -2,11 +2,16 @@ package com.example.finalproject;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,19 +24,48 @@ public class AddProjectDialog extends Dialog implements
     public Activity c;
     public Dialog d;
     public Button yes, no;
-
+    private Button red, yellow, green, orange;
     public AddProjectDialog(Activity a) {
         super(a);
         this.c = a;
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.add_project_dialog);
+        ConstraintLayout dialoglayout = (ConstraintLayout) findViewById(R.id.layout_dialog);
+        red = findViewById(R.id.btn_redButton);
+        yellow = findViewById(R.id.btn_yellowButton);
+        orange = findViewById(R.id.btn_orangeButton);
+        green = findViewById(R.id.btn_greenButton);
         yes = (Button) findViewById(R.id.add_dialog_btn);
         no = (Button) findViewById(R.id.cancel_dialog_btn);
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialoglayout.setBackgroundColor(Color.parseColor("#EF5959"));
+            }
+        });
+        orange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialoglayout.setBackgroundColor(Color.parseColor("#FF5722"));
+            }
+        });
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialoglayout.setBackgroundColor(Color.parseColor("#8BC34A"));
+            }
+        });
+        yellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialoglayout.setBackgroundColor(Color.parseColor("#FFEB3B"));
+            }
+        });
         yes.setOnClickListener(this);
         no.setOnClickListener(this);
     }
@@ -40,8 +74,16 @@ public class AddProjectDialog extends Dialog implements
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.add_dialog_btn:
-                addProject();
-                dismiss();
+                EditText title_field = findViewById(R.id.project_title_input);
+                String title = title_field.getText().toString();
+                if (title.isEmpty()){
+                    title_field.setError("Please enter project title");
+                    title_field.requestFocus();
+                }
+                else{
+                    addProject(title);
+                    dismiss();
+                }
                 break;
             case R.id.cancel_dialog_btn:
                 dismiss();
@@ -49,12 +91,11 @@ public class AddProjectDialog extends Dialog implements
             default:
                 break;
         }
-        dismiss();
     }
 
-    private void addProject() {
-        EditText title_field = findViewById(R.id.project_title_input);
-        String title = title_field.getText().toString();
+    private void addProject(String title) {
+
+
         Project project = new Project(title);
         DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
         String uid = "";
